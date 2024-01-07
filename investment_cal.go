9736bdb22ccd8a43a46fab2eebf130875b707788
Prashant Bhat <prashantbhat84@gmail.com>
2024-01-07 20:34:54 +0530

    added inflation

--- investment_cal.go
+++ investment_cal.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 6.5
 	// var investmentAmount float64 = 2000
 	investmentAmount, years := 2000.00, 10.00
 	expectedReturnRate := 5.5
 	// years := 10.0
 	// var futureValue = float64(investmentAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
 	futureValue := (investmentAmount) * math.Pow((1+expectedReturnRate/100), (years))
+	futureRealValue := futureValue / math.Pow((1+inflationRate/100), years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
